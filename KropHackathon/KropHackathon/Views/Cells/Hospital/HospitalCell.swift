315c72150a0e14821d6c0125378e78d369af6ee3
Alena Drobko <alena.drobko@onix-systems.com>
2019-12-04 11:36:47 +0200

    create ServiceCell

---
+++ KropHackathon/KropHackathon/Views/Cells/Hospital/HospitalCell.swift
@@ -0,0 +1,9 @@
+//
+//  HospitalCell.swift
+//  KropHackathon
+//
+//  Created by Альона Дробко on 04.12.2019.
+//  Copyright © 2019 onix. All rights reserved.
+//
+
+import Foundation
---
+++ KropHackathon/KropHackathon/Views/Cells/Service/ServiceTableViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  HospitalServiceTableViewCell.swift
+//  KropHackathon
+//
+//  Created by Альона Дробко on 03.12.2019.
+//  Copyright © 2019 onix. All rights reserved.
+//
+
+import UIKit
+
+class ServiceTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var name: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func configure(name: String, _ special: String = "") {
+        
+        let font = UIFont.init(name: "SFRounded-Ultralight", size: 17)
+        var attributes = [NSAttributedString.Key.font: font,
+            .foregroundColor: UIColor.black]
+        
+        let attributedName = NSAttributedString(string: name, attributes: attributes)
+        
+        attributes = [NSAttributedString.Key.font: font,
+            .foregroundColor: UIColor.lightGray]
+        
+        let attributedSpeck = NSAttributedString(string: special, attributes: attributes)
+        
+        let combination = NSMutableAttributedString()
+
+        combination.append(attributedSpeck)
+        combination.append(attributedName)
+        self.name.attributedText = combination
+        
+    }
+    
+}
