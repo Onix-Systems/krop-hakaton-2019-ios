15aa369cba988a3f0b4979ae0f7203e2f9e42ac8
Tetiana Nieizviestna <tatiana.neizvestnaya@onix-systems.com>
2019-12-05 17:11:24 +0200

    Fixed nav bar on HospitalDetails screen

--- KropHackathon/KropHackathon/Controllers/HospitalScreen/HospitalDetailesController.swift
+++ KropHackathon/KropHackathon/Controllers/HospitalScreen/HospitalDetailesController.swift
@@ -13,10 +13,10 @@ import GoogleMaps
 final class HospitalDetailesController: UIViewController {
     var viewModel: HospitalDetailesModelType!
     
-    @IBOutlet weak var lookOnMapButton: UIButton!
+    @IBOutlet private weak var lookOnMapButton: UIButton!
     @IBOutlet private weak var tableView: UITableView!
-    @IBOutlet weak var map: MapView!
-    
+    @IBOutlet private weak var map: MapView!
+    @IBOutlet private weak var closeButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,39 +41,28 @@ final class HospitalDetailesController: UIViewController {
         lookOnMapButton.layer.applySketchShadow(color: Style.Color.shadowColor, alpha: 0.14, xxx: 0, yyy: 4, blur: 12, spread: 0)
         
         tableView.register([HospitalDetailsTableViewCell.className, TitleTableViewCell.className])
-        tableView.setDataSource(self, delegate: self)
+        tableView.setDataSource(self)
         
         tableView.reloadData()
     }
     
     private func configureNavigationBar() {
-        navigationController?.isNavigationBarHidden = false
-        
-        let backBarButton = UIBarButtonItem(title: "", style: .plain, target: self, action: nil)
-        navigationItem.backBarButtonItem = backBarButton
-        
-        let btn = UIButton(type: .custom)
-        btn.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
-        btn.addTarget(self, action: #selector(backBtnClicked), for: .touchUpInside)
-        btn.setImage(Style.Images.backIcon, for: .normal)
-        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: btn)
-        
-        let titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width - 50, height: 50))
-        titleLabel.textColor = UIColor(red: 0.01, green: 0.10, blue: 0.19, alpha: 1.0)
-        titleLabel.font = UIFont.sfRoundedBold(17)
-        titleLabel.text = "Ультразвуковi дослiдження"
-        navigationItem.titleView = titleLabel
-        
+        navigationController?.isNavigationBarHidden = true
+        navigationController?.navigationBar.isTranslucent = true
+                
+    }
+    
+    @IBAction func closeBtnClicked(_ sender: UIButton) {
+        viewModel.goBack()
     }
     
     @objc
     func backBtnClicked() {
         viewModel.goBack()
-        //        print("backBtn clicked")
     }
 }
 
-extension HospitalDetailesController: UITableViewDataSource, UITableViewDelegate {
+extension HospitalDetailesController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel.hospitalInfoDetailModels.count + 1
