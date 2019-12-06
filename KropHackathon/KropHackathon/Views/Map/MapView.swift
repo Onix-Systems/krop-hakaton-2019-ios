b24ddcb26b7880c0ae8ae33e986ae98c27e4ad0b
Alena Drobko <alena.drobko@onix-systems.com>
2019-12-06 09:11:24 +0200

    change backButton on HospitalScreen

--- KropHackathon/KropHackathon/Views/Map/MapView.swift
+++ KropHackathon/KropHackathon/Views/Map/MapView.swift
@@ -9,17 +9,10 @@
 import UIKit
 import GoogleMaps
 
-protocol MapViewDelegate: class {
-    func didMarkerTapped(with coordinates: CLLocationCoordinate2D)
-    func didMapTapped()
-}
-
 class MapView: UIView {
     private let defaultLocation = CLLocation(latitude: 50.4547, longitude: 30.5238)
     
     @IBOutlet weak var ivMap: GMSMapView!
-    
-    weak var delegate: MapViewDelegate?
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -35,8 +28,6 @@ class MapView: UIView {
         ivMap.isMyLocationEnabled = true
         ivMap.settings.myLocationButton = false
         
-        ivMap.delegate = self
-        
         ivMap.animate(toZoom: 12)
         
         if let coordinate = coordinate {
@@ -51,37 +42,11 @@ class MapView: UIView {
         ivMap.animate(toLocation: location.coordinate)
     }
     
-    func moveToDefaultLocation() {
-        ivMap.animate(toZoom: 12)
-        ivMap.animate(toLocation: defaultLocation.coordinate)
-    }
-    
     func addMarker(coordinate: CLLocationCoordinate2D) {
         let marker = GMSMarker()
         marker.position = coordinate
         marker.icon = UIImage(named: "icPin")
         marker.map = ivMap
-    }
-
-    func moveToPoint(showInfo: Bool = true) {
-        ivMap.animate(toZoom: 12)
-        //ivMap.animate(toLocation: model.coordinate)
-    }
-    
-    func localize() {
-        
-    }
-    
-}
-
-extension MapView: GMSMapViewDelegate {
-    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
-        delegate?.didMarkerTapped(with: marker.position)
-        return true
-    }
-    
-    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
-        delegate?.didMapTapped()
     }
     
 }
