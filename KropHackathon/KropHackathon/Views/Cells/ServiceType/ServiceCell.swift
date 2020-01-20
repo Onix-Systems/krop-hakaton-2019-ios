ff71244a411b933d3e097ee8b7b469bbf5a12fbc
Alena Drobko <alena.drobko@onix-systems.com>
2020-01-20 22:23:56 +0200

    add tags

--- KropHackathon/KropHackathon/Views/Cells/ServiceType/ServiceCell.swift
+++ KropHackathon/KropHackathon/Views/Cells/ServiceType/ServiceCell.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import TagListView
 
 final class ServiceCell: UITableViewCell {
     
+    @IBOutlet weak var tagsListView: TagListView!
     @IBOutlet private weak var servicesLabel: UILabel!
     @IBOutlet private weak var wrapperView: UIView!
     @IBOutlet private weak var serviceTypeLabel: UILabel!
@@ -20,7 +22,7 @@ final class ServiceCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         serviceImageView.layer.cornerRadius = Style.Radius.defaultRadius
-        
+        tagsListView.delegate = self
         wrapperView.layer.cornerRadius = 11.0
 //        wrapperView.layer.borderWidth = 0.5
 //        wrapperView.layer.borderColor = Style.Color.borderColor.cgColor
@@ -30,23 +32,66 @@ final class ServiceCell: UITableViewCell {
     func configure(_ model: ServiceTypeModel) {
         self.model = model
         self.wrapperView.backgroundColor = model.backColor
+        tagsListView.addTags([])
         var i = 0
         var more = 0
+        var f: [String] = []
+        if tagsListView.isHidden {
+        
+           tagsListView.isHidden = false 
         let servises = model.services.prefix(while: { element -> Bool in
+            if element.count > 30 {
+                return true
+            }
             i += 1 + element.count
-            if i <= 30 { // Max length of line
+            if i <= getSize() { // Max length of line
                 more += 1
+                f.append(element)
                 return true
             } else {
                 return false
             }
         }).joined(separator: " | ")
+        
+        
+        
+        
         if model.services.count - more == 0 {
             self.servicesLabel.text = servises
         } else {
             self.servicesLabel.text = servises + " | інше(+\(model.services.count - more))"
+            f.append("Iнше(\(model.services.count - more))")
         }
+        
+        tagsListView.addTags([])
+        tagsListView.addTags(f)
+        
+        
+        
         self.serviceTypeLabel.text = model.name
         self.serviceImageView.image = UIImage(named: model.image) ?? UIImage(named: "redCross")
+        }
+    }
+    
+    
+    func getSize() -> Int {
+        switch UIScreen.main.nativeBounds.height {
+        case 1136:
+            return 30
+        case 1334:
+            return 60
+        case 1920, 2208:
+            return 95
+        case 2436:
+            return 60
+        case 2688:
+            return 95
+        case 1792:
+            return 95
+        default:
+            return 50
+        }
     }
 }
+
+extension ServiceCell: TagListViewDelegate {}
